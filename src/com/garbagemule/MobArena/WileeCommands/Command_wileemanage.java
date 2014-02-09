442b0aa816678f9db3c091e6733404d7992b4c75
Wilee999 <Infinitywilee@rocketmail.com>
2014-02-08 22:18:08 -0800

    Added some messages into /wm report to avoid confusion.

--- src/com/garbagemule/MobArena/WileeCommands/Command_wileemanage.java
+++ src/com/garbagemule/MobArena/WileeCommands/Command_wileemanage.java
@@ -589,6 +589,7 @@ public class Command_wileemanage extends MA_Command
             if (args.length == 1)
             {
                 MAUtils.adminbcastMsg(sender.getName() + " is getting griefed!", ChatColor.RED);
+                sender.sendMessage(ChatColor.GREEN + "The administration team has been notified that you are getting griefed. :)");
                 return true;
             }
 
@@ -603,6 +604,7 @@ public class Command_wileemanage extends MA_Command
             }
 
             MAUtils.adminbcastMsg(String.format("[GrieferReport:%s] %s", sender.getName(), message), ChatColor.RED);
+            sender.sendMessage(ChatColor.GREEN + "Your message has been sent to the administration team. :)");
             return true;
         }
 
